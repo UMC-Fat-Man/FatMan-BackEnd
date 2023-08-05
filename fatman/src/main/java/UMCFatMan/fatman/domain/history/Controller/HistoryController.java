c2d9d6c6c7e53cb3e0391fe2ad6485459f253425
홍다연 <95288297+Dayeon-Hong@users.noreply.github.com>
2023-08-06 05:48:20 +0900

    fix : controller url 앞에 api 붙여 수정

--- fatman/src/main/java/UMCFatMan/fatman/domain/history/Controller/HistoryController.java
+++ fatman/src/main/java/UMCFatMan/fatman/domain/history/Controller/HistoryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/history")
+@RequestMapping("/api/history")
 public class HistoryController {
 
     @Autowired
