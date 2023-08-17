561274d61bed12af9a9374e60d2a462978109046
KongJunWoo <joonoo3@naver.com>
2023-08-17 19:02:27 +0900

    feat : 회원가입 로직

--- fatman/src/main/java/UMCFatMan/fatman/Application.java
+++ fatman/src/main/java/UMCFatMan/fatman/Application.java
@@ -1,9 +1,14 @@
 package UMCFatMan.fatman;
 
+import env.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = {
+		"classpath:env/env.yml"
+		}, factory = EnvConfig.class)
 public class Application {
 
 	public static void main(String[] args) {
