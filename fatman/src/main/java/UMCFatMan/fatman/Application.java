1898b51667b1fde935a61170ef3f8071b0b6cf91
KongJunWoo <joonoo3@naver.com>
2023-08-22 14:37:09 +0900

    t

--- fatman/src/main/java/UMCFatMan/fatman/Application.java
+++ fatman/src/main/java/UMCFatMan/fatman/Application.java
@@ -1,14 +1,9 @@
 package UMCFatMan.fatman;
-
-import env.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = {
-		"classpath:env.yml"
-		}, factory = EnvConfig.class)
 public class Application {
 
 	public static void main(String[] args) {
