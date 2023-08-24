d082fc1c59fd19360aafe570526d5fc37056c65c
KongJunWoo <joonoo3@naver.com>
2023-08-25 01:04:10 +0900

    feat : 자동

--- fatman/src/main/java/UMCFatMan/fatman/global/oauth2/CustomOAuth2SuccessHandler.java
+++ fatman/src/main/java/UMCFatMan/fatman/global/oauth2/CustomOAuth2SuccessHandler.java
@@ -1,17 +1,12 @@
 package UMCFatMan.fatman.global.oauth2;
 
-import UMCFatMan.fatman.domain.users.dto.UserEmailNameDto;
 import UMCFatMan.fatman.domain.users.entity.Users;
-import UMCFatMan.fatman.domain.users.mapper.UserMapper;
 import UMCFatMan.fatman.domain.users.repository.UsersRepository;
 import UMCFatMan.fatman.global.exception.user.UserNotFoundException;
 import UMCFatMan.fatman.global.jwt.JWTUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -20,12 +15,11 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Optional;
 
 // OAuth2 로그인 성공 시 처리할 로직을 정의하는 핸들러
-@Slf4j
 @Component
 public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler {
+
     @Autowired
     private JWTUtil jwtUtil; // JWT 생성 유틸리티
 
@@ -34,23 +28,12 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-
         // OAuth2 로그인 성공한 사용자 정보 가져오기
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         String email = oAuth2User.getAttribute("email");
-        String name = oAuth2User.getAttribute("name");
 
-        log.info("onAuthenticationSuccess"+email);
         // DB에서 사용자 조회
-        Users user = userRepository.findByEmail(email).orElseGet(()->{
-            UserEmailNameDto userEmailNameDto = new UserEmailNameDto();
-            userEmailNameDto.setEmail(email);
-            userEmailNameDto.setName(name);
-            Users Changeduser = UserMapper.toUserEntity(userEmailNameDto);
-            log.info(Changeduser.getEmail());
-            userRepository.save(Changeduser);
-            return Changeduser;
-        });
+        Users user = (Users) userRepository.findByEmail(email).orElseThrow(UserNotFoundException::new);
 
         // JWT 토큰 생성
         String token = jwtUtil.makeAuthToken(user);
